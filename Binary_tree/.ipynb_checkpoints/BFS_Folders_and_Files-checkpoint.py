81f9e9db9efa94142eb3cde0d7b5f925f4ec65a6
FatmaElMahdi1000 <f69246984@gmail.com>
2025-08-31 08:42:35 +0300

    BFS Algorithm and Binart trees!

---
+++ Binary_tree/.ipynb_checkpoints/BFS_Folders_and_Files-checkpoint.py
@@ -0,0 +1,18 @@
+from collections import deque 
+from os import listdir
+from os.path import join, isfile, isdir
+
+def printing_names(start_dir):
+    search_queue = deque()
+    search_queue.append(start_dir)
+    
+    while search_queue:
+        current_dir = search_queue.popleft()
+        for File in listdir(current_dir):
+            full_path = join(current_dir, File)
+            if isfile(full_path):
+                print(f"File Name: {full_path}")
+            elif isdir(full_path):
+                search_queue.append(full_path)
+
+print(printing_names(".")) #with listdir giving the method this "." means we're working on the current directory       
